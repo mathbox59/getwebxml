35cbc5b71a225d070e54c7aa358dfd36540a093f
pipimushroom <viviliuyuwei@gmail.com>
2013-07-08 13:42:44 +0800

    columnview

--- GetWebXML/GetWebXML/GetWebXML/GetWebXML/View/Columnview.xaml.cs
+++ GetWebXML/GetWebXML/GetWebXML/GetWebXML/View/Columnview.xaml.cs
@@ -11,6 +11,46 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using Visifire.Charts;
+using GetWebXML.Parser;
+
+namespace GetWebXML.View
+{
+    public partial class Columnview : PhoneApplicationPage
+    {
+        BasicChartGeneric bc_column=new BasicChartGeneric();
+        Node_BasicChart nbc_column = new Node_BasicChart();
+        public Columnview()
+        {
+            InitializeComponent();
+            
+        }
+        public Columnview(Node_BasicChart nodebasicchart, BasicChartGeneric bcview)
+        {
+            bc_column = bcview;
+            nbc_column = nodebasicchart;
+        }
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Create new DataSeries
+            DataSeries dataseries = new DataSeries();
+            for(int index=0;index<nbc_column.ChartOptions.xAxis.categories.Length;index++)
+            dataseries.DataPoints.Add(new DataPoint{})
+        
+        }
+    }
+}using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Visifire.Charts;
 
 namespace GetWebXML.View
 {
