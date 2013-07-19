f1e360c5f1ddddaecb90e9f98973172f809a906a
pipimushroom <viviliuyuwei@gmail.com>
2013-07-19 09:53:50 +0800

    initializacomponent

--- GetWebXML/GetWebXML/GetWebXML/GetWebXML/MainPage.xaml.cs
+++ GetWebXML/GetWebXML/GetWebXML/GetWebXML/MainPage.xaml.cs
@@ -18,6 +18,7 @@ using GetWebXML.DES;
 using GetWebXML.Parser;
 using GetWebXML.View;
 using GetWebXML.core;
+using System.Threading;
 //using GetWebXML.Control;
 
 namespace GetWebXML
@@ -26,17 +27,15 @@ namespace GetWebXML
     {
         string stringJson;
         string url;
-        int a = 0;
         string lname = "SampleScript.txt";
         string json4lua;
+        AutoResetEvent myEvent = new AutoResetEvent(false);
+
         // 构造函数
         public MainPage()
-        {    //killlllllllllllllllllllllllllllllllllllllllllllllllllll
-            //meeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        {  
             InitializeComponent();
             //url = "{\"url\":\"DROPDOWN/DROPDOWN_1\"}";
-            //Hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-            //Hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
             //url = "{\"url\":\"BL/BL_1\"}"; 
             url = "{\"url\":\"LYW/LYW_1\"}";
             getlua lua = new getlua(lname);
@@ -61,111 +60,145 @@ namespace GetWebXML
                         //解密返回json
                         stringJson = Decryption_Base64.Base64Decode(arg.Result);
                         // json 格式化
-                       stringJson=FormatJson(stringJson);
-                       System.Diagnostics.Debug.WriteLine(stringJson);
-                        //解析
-                        NodeInfo nodeInfo = JsonParser.Deserialize<NodeInfo>(stringJson);                       
-                        System.Diagnostics.Debug.WriteLine(nodeInfo.message);
-                        //获取所有控件类型
-                        List<string> ctltype=new List<string>();
-                        if (nodeInfo.data.ControlCount == 1) {
-                            int indexstart = stringJson.IndexOf("ControlType" );
-                            int indexend = stringJson.IndexOf("ControlCount" );
-                            string ss = stringJson.Substring(indexstart - 2, indexend - indexstart - 1);
-                            ctltype.Add(ss);
-                        }else
-                        {
-                            int tempstart = 0;
-                            for (int i = 0; i < nodeInfo.data.ControlCount; i++) 
-                            {
-                                if (i == nodeInfo.data.ControlCount-1)
-                                {
-                                    int start = stringJson.IndexOf("ControlType", 1 + tempstart);
-                                    int end = stringJson.IndexOf("ControlCount");
-                                    string sl = stringJson.Substring(start - 2, end - start - 1);
-                                    ctltype.Add(sl);
-                                    break;
-                                }
-                                else
-                                {
-                                    int indexstart = stringJson.IndexOf("ControlType", 1 + tempstart);
-                                    int indexend = stringJson.IndexOf("ControlType", 1 + indexstart);
-                                    tempstart = indexstart;
-                                    string ss = stringJson.Substring(indexstart - 2, indexend - indexstart - 1);
-                                    ctltype.Add(ss);
-                                }
-                            } 
-                        }
+                        FormatJson();
+            //invokewebservice();
+            //myEvent.WaitOne();
+            //解析
+            NodeInfo nodeInfo = JsonParser.Deserialize<NodeInfo>(stringJson);
+            System.Diagnostics.Debug.WriteLine(nodeInfo.message);
+            //获取所有控件类型
+            List<string> ctltype = new List<string>();
+            if (nodeInfo.data.ControlCount == 1)
+            {
+                int indexstart = stringJson.IndexOf("ControlType");
+                int indexend = stringJson.IndexOf("ControlCount");
+                string ss = stringJson.Substring(indexstart - 2, indexend - indexstart - 1);
+                ctltype.Add(ss);
+            }
+            else
+            {
+                int tempstart = 0;
+                for (int i = 0; i < nodeInfo.data.ControlCount; i++)
+                {
+                    if (i == nodeInfo.data.ControlCount - 1)
+                    {
+                        int start = stringJson.IndexOf("ControlType", 1 + tempstart);
+                        int end = stringJson.IndexOf("ControlCount");
+                        string sl = stringJson.Substring(start - 2, end - start - 1);
+                        ctltype.Add(sl);
+                        break;
+                    }
+                    else
+                    {
+                        int indexstart = stringJson.IndexOf("ControlType", 1 + tempstart);
+                        int indexend = stringJson.IndexOf("ControlType", 1 + indexstart);
+                        tempstart = indexstart;
+                        string ss = stringJson.Substring(indexstart - 2, indexend - indexstart - 1);
+                        ctltype.Add(ss);
+                    }
+                }
+            }
 
-                        //本地生成控件
-                        for (int i = 0; i < nodeInfo.data.ControlCount; i++) 
+            //本地生成控件
+            for (int i = 0; i < nodeInfo.data.ControlCount; i++)
+            {
+                switch (nodeInfo.data.ControlList[i].ControlType)
+                {
+                    case "DropDownList":
+                        Node_Dropdownlist nodedropdown = JsonParser.Deserialize<Node_Dropdownlist>(ctltype.ElementAt(i));
+                        ListPicker dropdownlist = new ListPicker();
+                        DropDownListView dropdownlistview = new DropDownListView(dropdownlist, nodedropdown);
+                        dropdownlist = dropdownlistview.Dropdownlistconfig();
+                        //添加到界面
+                        this.panel.Children.Add(dropdownlist);
+                        //生成下拉菜单控件
+                        System.Diagnostics.Debug.WriteLine(nodedropdown.ControlType);
+                        break;
+                    case "BasicChart":
+                        Node_BasicChart nodebasicchart = JsonParser.Deserialize<Node_BasicChart>(ctltype.ElementAt(i));
+                        Position basicchart_p = new Position(nodeInfo, i);
+                        BasicChartGeneric bcview = new BasicChartGeneric(basicchart_p);
+                        switch (nodebasicchart.SeriesData[0].type)
                         {
-                            switch (nodeInfo.data.ControlList[i].ControlType) 
-                            {
-                                case "DropDownList":
-                                    Node_Dropdownlist nodedropdown = JsonParser.Deserialize<Node_Dropdownlist>(ctltype.ElementAt(i));
-                                    ListPicker dropdownlist = new ListPicker();
-                                    DropDownListView dropdownlistview = new DropDownListView( dropdownlist, nodedropdown);
-                                    dropdownlist=dropdownlistview.Dropdownlistconfig();
-                                    //添加到界面
-                                    this.panel.Children.Add(dropdownlist);
-                                    //生成下拉菜单控件
-                                    System.Diagnostics.Debug.WriteLine(nodedropdown.ControlType);
-                                    break;
-                                case "BasicChart":
-                                    Node_BasicChart nodebasicchart = JsonParser.Deserialize<Node_BasicChart>(ctltype.ElementAt(i));
-                                    Position basicchart_p = new Position(nodeInfo, i);
-                                    BasicChartGeneric bcview = new BasicChartGeneric(basicchart_p);
-                                    switch (nodebasicchart.SeriesData[0].type) 
-                                    {
-                                        case "column":
-                                            this.NavigationService.Navigate(new Uri("/View/Columnview.xaml", UriKind.Relative));
-                                            Columnview columnview = new Columnview(nodebasicchart, bcview);  
-                                            break;
-                                        case "area":
-                                            break;
-                                        case "line":
-                                            this.NavigationService.Navigate(new Uri("/View/ColumnChart.xaml", UriKind.Relative));
-                                            break;
-                                        case "bar":
-                                            break;
-                                        case "pie":
-                                            break;
-                                    }
-                                    System.Diagnostics.Debug.WriteLine(nodebasicchart.ControlID);
-                                    
-                                    break;
-                                case "DataGrid":
-                                    Node_DataGrid nodedatagrid = JsonParser.Deserialize<Node_DataGrid>(ctltype.ElementAt(i));
-                                    System.Diagnostics.Debug.WriteLine(nodedatagrid.ControlID);
-                                    break;
-                                case "Label":
-                                    Node_Label nodelabel = JsonParser.Deserialize<Node_Label>(ctltype.ElementAt(i));
-                                    TextBlock label = new TextBlock();
-                                    LabelView labelview = new LabelView(label, nodelabel);
-                                    label = labelview.Labelconfig();
-                                    this.panel.Children.Add(label);
-                                    System.Diagnostics.Debug.WriteLine(nodeInfo.data.ControlList[i].ControlType);
-                                    break;
-                            }
+                            case "column":
+                                this.NavigationService.Navigate(new Uri("/View/Columnview.xaml", UriKind.Relative));
+                                Columnview columnview = new Columnview(nodebasicchart, bcview);
+
+                                break;
+                            case "area":
+                                break;
+                            case "line":
+                                this.NavigationService.Navigate(new Uri("/View/ColumnChart.xaml", UriKind.Relative));
+                                break;
+                            case "bar":
+                                break;
+                            case "pie":
+                                break;
                         }
-                    
-                    }            
+                        System.Diagnostics.Debug.WriteLine(nodebasicchart.ControlID);
+
+                        break;
+                    case "DataGrid":
+                        Node_DataGrid nodedatagrid = JsonParser.Deserialize<Node_DataGrid>(ctltype.ElementAt(i));
+                        System.Diagnostics.Debug.WriteLine(nodedatagrid.ControlID);
+                        break;
+                    case "Label":
+                        Node_Label nodelabel = JsonParser.Deserialize<Node_Label>(ctltype.ElementAt(i));
+                        TextBlock label = new TextBlock();
+                        LabelView labelview = new LabelView(label, nodelabel);
+                        label = labelview.Labelconfig();
+                        this.panel.Children.Add(label);
+                        System.Diagnostics.Debug.WriteLine(nodeInfo.data.ControlList[i].ControlType);
+                        break;
+                }
+            }
+
+                    }
                 };
             //调用异步方法
             textstate.Text = "正在请求，请等候……";
             MyClient.IOS_VSFileReadByIDAsync(url);
         }
         // json 格式化
-        public string FormatJson(string stringJson)
+        public void FormatJson()
         {
            stringJson = stringJson.Replace("\\", "");
            stringJson = stringJson.Replace("\"{", "{");
            stringJson = stringJson.Replace("}\"", "}");
            stringJson = stringJson.Replace("\"[", "[");
            stringJson = stringJson.Replace("]\"", "]");
-           return stringJson;
+           //return stringJson;
         }
+        //public void invokewebservice()
+        //{
 
+        //    ServiceReference1.Service1SoapClient MyClient = new ServiceReference1.Service1SoapClient();
+        //    //调用异步方法
+        //    textstate.Text = "正在请求，请等候……";
+        //    MyClient.IOS_VSFileReadByIDAsync(url);
+     
+        //    // 绑定回调事件
+        //    MyClient.IOS_VSFileReadByIDCompleted += (s, arg) =>
+        //    {
+        //        textstate.Text = "请求完成";
+        //        if (arg.Error != null)
+        //        {
+        //            System.Diagnostics.Debug.WriteLine(arg.Error);
+        //            return;
+        //        }
+        //        if (arg.Result != null)
+        //        {
+        //            System.Diagnostics.Debug.WriteLine("**********Result**********");
+        //            //解密返回json
+        //            stringJson = Decryption_Base64.Base64Decode(arg.Result);
+        //            // json 格式化
+        //            FormatJson();
+        //            System.Diagnostics.Debug.WriteLine(stringJson);
+                    
+        //        }
+
+        //    };
+         
+        //}
     }
  }
--- GetWebXML/GetWebXML/GetWebXML/GetWebXML/View/Columnview.xaml.cs
+++ GetWebXML/GetWebXML/GetWebXML/GetWebXML/View/Columnview.xaml.cs
@@ -21,25 +21,40 @@ namespace GetWebXML.View
         Node_BasicChart nbc_column = new Node_BasicChart();
         public Columnview()
         {
-            InitializeComponent();
-            CreatChart();
-            
+            //InitializeComponent();
+            //Visifire.Charts.Chart chart = new Visifire.Charts.Chart();
+            ////DataSeries dataseries = new DataSeries { RenderAs = RenderAs.Column };
+            ////DataSeries dataseries = new DataSeries { RenderAs = RenderAs.Area };
+            //DataSeries dataseries = new DataSeries { RenderAs = RenderAs.Line };
+            //// DataSeries dataseries = new DataSeries { RenderAs = RenderAs.Pie };
+            //dataseries.DataPoints.Add(new DataPoint { AxisXLabel = "07:00", YValue = 53, XValue = 0 });
+            //dataseries.DataPoints.Add(new DataPoint { AxisXLabel = "08:00", YValue = 73, XValue = 1 });
+            //dataseries.DataPoints.Add(new DataPoint { AxisXLabel = "09:00", YValue = 123, XValue = 2 });
+            //dataseries.DataPoints.Add(new DataPoint { AxisXLabel = "10:00", YValue = 33, XValue = 3 });
+            //chart.Series.Add(dataseries);
+            //LayoutRoot.Children.Add(chart);
+            //System.Diagnostics.Debug.WriteLine("killlllll meeeeeeee");
         }
         public Columnview(Node_BasicChart nodebasicchart, BasicChartGeneric bcview)
         {
+            InitializeComponent();
             bc_column = bcview;
             nbc_column = nodebasicchart;
+            CreatChart();
         }
         public void CreatChart()
         {
             Visifire.Charts.Chart chart = new Visifire.Charts.Chart();
             // Create new DataSeries
             DataSeries dataseries = new DataSeries();
+            //int num = nbc_column.ChartOptions.xAxis.categories.Length;
             for(int index=0;index<nbc_column.ChartOptions.xAxis.categories.Length;index++)
             {
-                dataseries.DataPoints.Add(new DataPoint { AxisXLabel = nbc_column.ChartOptions.xAxis.categories[index], YValue = 53});
+                dataseries.DataPoints.Add(new DataPoint { AxisXLabel = nbc_column.ChartOptions.xAxis.categories[index], YValue = 53+index*10, XValue = index });
             }
+            
             chart.Series.Add(dataseries);
+            LayoutRoot.Children.Add(chart);
         }
     }
 }
